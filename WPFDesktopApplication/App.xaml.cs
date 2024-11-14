4af577173ffdbac7afa548c5b5ad198b3d34b931
ulises <ulises.sandria@brendcar.com>
2024-11-14 10:04:39 -0600

    Agregar archivos de proyecto.

---
+++ WPFDesktopApplication/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPFDesktopApplication
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
